a3ac4b05125d1068ca8c6094820fdf7e7c968095
stephane <stephane.puechmorel@enac.fr>
2023-10-10 15:06:37 +0200

    Version avec shell de test.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 
 int main(int argc, char *argv[]) {
     int n = 1000000;
-    if (launch_and_test(n) == 0)
+    int res = 0;
+    if (launch_and_test(n) == 0) {
         printf("Echec.\n");
+        res = 1;
+    }
     else 
         printf("Vérifié.\n");
-    if (add_unified(n) == 0)
+    if (add_unified(n) == 0) {
         printf("Echec.\n");
+        res = 1;
+    }
     else 
         printf("Vérifié.\n");
+    return res;
 }
