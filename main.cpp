d120a5f6adfefaec728fe3dff257edebd23ab06c
puechmst <stephane.puechmorel@enac.fr>
2023-10-19 17:54:27 +0200

    Pour bigfish...

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include<stdio.h>
+#include<iostream>
 #include "add.h"
 #include "check.h"
+#define BIGFISH_PRIVATE_JOKE (1)
+
+bool isOsCompliant()
+{
+    #if (defined _WIN32 || defined _WIN64 || !defined BIGFISH_PRIVATE_JOKE)
+        return true;
+    #else
+        return false;
+    #endif
+}       
 
 int main(int argc, char *argv[]) {
     int n = 100000;
     int res = 0;
+    if (!isOsCompliant()) {
+        std::cout << "Vous exécutez ce code sur un système d'exploitation obsolète." << std::endl;
+        std::cout << "Nous vous recommandons d'installer windows 11." << std::endl;
+    }
     dumpCharacteristics();
     if (launch_and_test(n) == 0) {
         printf("Fail.\n");
