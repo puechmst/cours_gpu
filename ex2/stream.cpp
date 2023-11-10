54d125f2bed76e94e1d17527f76da7712dbeea74
puechmst <stephane.puechmorel@enac.fr>
2023-11-10 07:34:19 +0100

    Produit scalaire terminé. Début de l'écriture de la partie flux.

--- ex2/stream.cpp
+++ ex2/stream.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<stddef.h>
+#include"stream.h"
 
 int main(int argc, char *argv[]) {
-    
+    test_dot(256 * 50);
     return EXIT_SUCCESS;
 }
---
+++ ex2/stream.cu
@@ -0,0 +1,77 @@
+
+#include<cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <iostream>
+// la taille d'un bloc doit être un multiple de 2
+#define BLOCK_DIM (256)
+
+// note: un noyau ne peut pas retourner de valeur. Il faut la récupérer à l'aide d'une fonction spéciale
+__device__ float res;
+
+__global__ void dot_product(float *a, float *b, float *c, int  n) {
+    __shared__ float r[BLOCK_DIM];
+    int i = blockIdx.x * BLOCK_DIM + threadIdx.x;
+    int step = BLOCK_DIM >> 1;
+    float *c;
+    // allocation mémoire globale
+    if(i == 0)
+        cudaMalloc(&c, BLOCK_DIM);
+    // chargement des données
+    if(i < n) {
+        r[threadIdx.x] = a[i] * b[i];
+    } else
+        r[threadIdx.x] = 0.0f;
+    __syncthreads();
+    // calcul de la somme
+    while(step >= 1) {
+        if(threadIdx.x < step)
+           r[threadIdx.x] += r[threadIdx.x + step];
+        __syncthreads();
+        step >>= 1;
+    }
+    c[blockIdx.x] = r[0];
+    // final reduction
+    __syncthreads();
+    if(i == 0) { 
+        res = 0.0f;
+        for(int k = 0 ; k < BLOCK_DIM ; k++)
+            res += c[k];
+    }
+ }
+
+void test_dot(int n) {
+    float *a, *b, *c;
+    float *da, *db, *dc;
+    float dot[1];
+    a = new float[n];
+    b = new float[n];
+    int sz = (n+BLOCK_DIM -1)/BLOCK_DIM;
+    c = new float[sz];
+    for(int i = 0 ; i < n ; i++) {
+        a[i] = 1.0;
+        b[i] = (float)i;
+    }
+    cudaMalloc(&da, n * sizeof(float));
+    cudaMalloc(&db, n * sizeof(float));
+    cudaMalloc(&dc, sz * sizeof(float));
+    cudaMemcpy(da, a, n * sizeof(float),cudaMemcpyHostToDevice);
+    cudaMemcpy(db, b, n * sizeof(float),cudaMemcpyHostToDevice);
+    dot_product<<<sz,BLOCK_DIM>>>(da, db, dc, n);
+    cudaMemcpyFromSymbol(dot, res, sizeof(float));
+    cudaMemcpy(c, dc, sz * sizeof(float),cudaMemcpyDeviceToHost);
+    float r = 0.0f;
+    float r1 = 0.0f;
+    for(int i = 0 ; i < n ; i++) 
+            r += a[i] * b[i];
+    for(int i = 0 ; i < sz ; i++)
+            r1 += c[i];
+    std::cout << *dot << '\t' << r1 << '\t' << r <<std::endl;
+    cudaFree(da);
+    cudaFree(db);
+    cudaFree(dc);
+    delete []a;
+    delete []b;
+    delete []c;
+}
+ 
