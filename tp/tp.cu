b7096e7dc05b9cf5fdc3b774ff7b6026b2ba2194
stephane <stephane.puechmorel@enac.fr>
2023-12-09 16:18:28 +0100

    Version de la convolution 1D avec mémoire partagée.

--- tp/tp.cu
+++ tp/tp.cu
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 
-#define SIZE (100)
+#define SIZE (1000)
 #define HSIZE (10)
 #define NROW (100)
 #define NCOL (100)
@@ -57,6 +57,39 @@ __global__ void gpu_conv1d(int n, float *x, int p, float *h, float *y)
    }
 } 
 
+__global__ void gpu_conv1d_shared(int n, float *x, int p, float *h, float *y)
+{
+   int ib = blockIdx.x * BSIZE;
+   int i = ib + threadIdx.x;
+   int step = HSIZE - 1;
+   __shared__ float hs[HSIZE];
+   __shared__ float xs[BSIZE + HSIZE - 1];
+
+   if(threadIdx.x > 0 && i < n)
+      xs[threadIdx.x + step] = x[i];
+   else {
+      xs[threadIdx.x + step] = 0.0f;
+   }
+   if(threadIdx.x < HSIZE) {
+      hs[threadIdx.x] = h[step-threadIdx.x];
+      if(i >= step)
+         xs[threadIdx.x] = x[i - step];
+      else 
+         xs[threadIdx.x] = 0.0f;
+   }
+   __syncthreads();
+
+   if (i < n)
+   {
+      float s = 0.0;
+      for (int j = 0; j < HSIZE; j++)
+      {
+         s += xs[j+threadIdx.x] * hs[j];
+      }
+      y[i] = s;
+   }
+} 
+
 __global__ void gpu_conv2d(int m, int n, float *x, int p, int q, float *h, float *y)
 {
    int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -75,12 +108,13 @@ __global__ void gpu_conv2d(int m, int n, float *x, int p, int q, float *h, float
 } 
 
 float test_conv1d() {
-   float *x, *y, *h;
+   float *x, *y, *y0, *h;
    float *dx, *dy, *dh;
    int nb;
 
    x = new float[SIZE];
    y = new float[SIZE];
+   y0 = new float[SIZE];
    h = new float[HSIZE];
 
    for (int i = 0; i < SIZE; i++)
@@ -95,28 +129,31 @@ float test_conv1d() {
    cudaMalloc(&dh, HSIZE * sizeof(float));
    cudaMemcpy(dx, x, SIZE * sizeof(float), cudaMemcpyHostToDevice);
    cudaMemcpy(dh, h, HSIZE * sizeof(float), cudaMemcpyHostToDevice);
-   cpu_conv1d(SIZE, x, HSIZE, h, y);
-   gpu_conv1d<<<nb, BSIZE>>>(SIZE, dx, HSIZE, dh, dy);
-   cudaMemcpy(x, dy, SIZE * sizeof(float), cudaMemcpyDeviceToHost);
-   float err = 0.0f;
-   for (int i = 0; i < SIZE; i++)
-      err += fabsf(y[i] - x[i]);
+   gpu_conv1d_shared<<<nb, BSIZE>>>(SIZE, dx, HSIZE, dh, dy);
+   cudaMemcpy(y0, dy, SIZE * sizeof(float), cudaMemcpyDeviceToHost);
+   cudaDeviceSynchronize();
    cudaFree(dx);
    cudaFree(dy);
    cudaFree(dh);
+   cpu_conv1d(SIZE, x, HSIZE, h, y);
+   float err = 0.0f;
+   for (int i = 0; i < SIZE; i++)
+      err += fabsf(y[i] - y0[i]);
    delete[] x;
    delete[] y;
+   delete[] y0;
    delete[] h;
    return err;
 }
 
 float test_conv2d() {
-   float *x, *y, *h;
+   float *x, *y, *y0, *h;
    float *dx, *dy, *dh;
    int nbx, nby;
 
    x = new float[NROW * NCOL];
    y = new float[NROW * NCOL];
+   y0 = new float[NROW * NCOL];
    h = new float[NRH * NCH];
 
    for (int i = 0; i < NROW * NCOL; i++)
@@ -132,17 +169,19 @@ float test_conv2d() {
    cudaMalloc(&dh, NRH * NCH * sizeof(float));
    cudaMemcpy(dx, x, NROW * NCOL * sizeof(float), cudaMemcpyHostToDevice);
    cudaMemcpy(dh, h, NRH * NCH * sizeof(float), cudaMemcpyHostToDevice);
-   cpu_conv2d(NROW, NCOL, x, NRH, NCH, h, y);
    gpu_conv2d<<<dim3(nbx, nby, 1), dim3(BX, BY, 1)>>>(NROW, NCOL, dx, NRH, NCH, dh, dy);
-   cudaMemcpy(x, dy, NROW * NCOL * sizeof(float), cudaMemcpyDeviceToHost);
+   cudaMemcpy(y0, dy, NROW * NCOL * sizeof(float), cudaMemcpyDeviceToHost);
+   cudaDeviceSynchronize();
+   cpu_conv2d(NROW, NCOL, x, NRH, NCH, h, y);
    float err = 0.0f;
    for (int i = 0; i < NROW * NCOL; i++)
-      err += fabsf(y[i] - x[i]);
+      err += fabsf(y[i] - y0[i]);
    cudaFree(dx);
    cudaFree(dy);
    cudaFree(dh);
    delete[] x;
    delete[] y;
+   delete[] y0;
    delete[] h;
    return err;
 }
@@ -151,5 +190,6 @@ float test_conv2d() {
 int main(int argc, char *argv[])
 {
    std::cout << test_conv1d() << '\t' << test_conv2d()<< std::endl;
+   cudaDeviceReset();
    return 0;
 }
