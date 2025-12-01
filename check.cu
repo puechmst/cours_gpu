f31cda898da7ce5604017fb20abcd5828e907058
stephane <stephane.puechmorel@enac.fr>
2025-12-01 15:01:55 +0100

    Session 2025 - nettoyage.

--- check.cu
+++ check.cu
@@ -7,26 +7,20 @@ void dumpCharacteristics() {
        size_t free, total;
        int attr;
        int ndevices;
+       cudaDeviceProp prop;
        cudaGetDeviceCount(&ndevices);
        for(int device = 0 ; device < ndevices ; device++) {
             std::cout << "=============================================" << std::endl;
+            cudaGetDeviceProperties(&prop, device);
             std::cout << "Device " << device << std::endl;
-            cudaDeviceGetAttribute(&attr, cudaDevAttrMaxThreadsPerBlock,device);
-            std::cout << "Max threads per block : " << attr << std::endl;
-            cudaDeviceGetAttribute(&attr, cudaDevAttrWarpSize,device);
-            std::cout << "Warp size : " << attr << std::endl;
-            cudaDeviceGetAttribute(&attr, cudaDevAttrMaxBlockDimX,device);
-            std::cout << "Maximum bloc size in x coordinate : " << attr << std::endl;
-            cudaDeviceGetAttribute(&attr, cudaDevAttrMaxBlockDimY,device);
-            std::cout << "Maximum bloc size in y coordinate : " << attr << std::endl;
-            cudaDeviceGetAttribute(&attr, cudaDevAttrMaxBlockDimZ,device);
-            std::cout << "Maximum bloc size in z coordinate : " << attr << std::endl;
-            cudaDeviceGetAttribute(&attr, cudaDevAttrMaxGridDimX,device);
-            std::cout << "Maximum grid size in x coordinate : " << attr << std::endl;
-            cudaDeviceGetAttribute(&attr, cudaDevAttrMaxGridDimY,device);
-            std::cout << "Maximum grid size in y coordinate : " << attr << std::endl;
-            cudaDeviceGetAttribute(&attr, cudaDevAttrMaxGridDimZ,device);
-            std::cout << "Maximum grid size in z coordinate : " << attr << std::endl;
+            std::cout << "Max threads per block : " << prop.maxThreadsPerBlock << std::endl;
+            std::cout << "Warp size : " << prop.warpSize << std::endl;
+            std::cout << "Maximum bloc size in x coordinate : " << prop.maxThreadsDim[0] << std::endl;
+            std::cout << "Maximum bloc size in y coordinate : " << prop.maxThreadsDim[1] << std::endl;
+            std::cout << "Maximum bloc size in z coordinate : " << prop.maxThreadsDim[2] << std::endl;
+            std::cout << "Maximum grid size in x coordinate : " << prop.maxGridSize[0] << std::endl;
+            std::cout << "Maximum grid size in y coordinate : " << prop.maxGridSize[1] << std::endl;
+            std::cout << "Maximum grid size in z coordinate : " << prop.maxGridSize[2] << std::endl;
             std::cout << "=============================================" << std::endl;
        }
 
